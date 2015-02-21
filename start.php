af751596c3e67d5f8c9fd3c7419c56de18653d36
Gerard Kanters <gkanters@gmail.com>
2015-02-21 11:52:20 +0100

    Add login buttons
    Add login buttons to register page

--- start.php
+++ start.php
@@ -4,5 +4,5 @@
 	function elgg_social_login_init()
 	{
 		elgg_extend_view( 'forms/login'   , 'elgg_social_login/login' );
-//		elgg_extend_view( 'forms/register', 'elgg_social_login/login' );
+		elgg_extend_view( 'forms/register', 'elgg_social_login/login' );
 	}
